a9a39568c12915a6c5b998d8315e74af8cc83571
vjeuel <vjeuel@gmail.com>
2020-05-29 03:03:53 -0500

    fixing titles and subtitles

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,46 @@
 function generateMarkdown(answers) {
   return `
-#${answers.title}\n
+# ${answers.title}\n
 <p align="center">
   <img src="${answers.banner}" height="100%">
 </p>
 // Badge\n
 You can access the app [here](${answers.appLink})\n
+
 ### Description\n
 ${answers.description}
 <hr/>
+
 ### Contents\n
 ${answers.contents}
 <hr/>
+
 ### Install\n
 ${answers.install}
 <hr/>
+
 ### Usage\n
 ${answers.usage}
 <hr/>
+
 ### License\n
 ${answers.license}
 <hr/>
+
 ### Contributors\n
 ${answers.contributors}
 <hr/>
+
 ### Tests\n
 ${answers.tests}
 <hr/>
+
 ${answers.picture}
 <hr/>
+
 ${answers.email}
 <hr/>
+
 ${answers.username}
 `;
 }
